7e495eb96ca9c84c24ede9fc90eb1491c9beca7d
Rev <64957743+Rev@users.noreply.github.com>
2022-05-29 17:14:15 +0800

    Display stuff

--- Players.h
+++ Players.h
@@ -5,7 +5,7 @@
 #ifndef PLAYERS_H
 #define PLAYERS_H
 
-#include "Drawing.h"
+#include "ESP.h"
 
 bool isESP;
 bool isESPLine;
@@ -17,9 +17,9 @@ bool isESPKillDistance;
 int (*Display_get_height)(void *);
 int (*Display_get_width)(void *);
 void *(*Display_get_main)();
+void (*Screen_SetResolution)(int width, int height, bool fullscreen);
 
 Vector3 (*WorldToScreenPoint)(void *instance, Vector3);
-void (*Screen_SetResolution)(int width, int height, bool fullscreen);
 Vector3 (*Transform_get_position)(void *instance);
 void (*get_position)(void *instance, Vector3 *_vectorInstance);
 void *(*get_main)();
@@ -30,6 +30,11 @@ monoString *(*PlayerName)(void *instance);
 
 void AttachFunctions()
 {
+    Display_get_height = (int (*)(void*)) offsets->Display_get_systemHeight;
+    Display_get_width = (int (*)(void*)) offsets->Display_get_systemWidth;
+    Display_get_main = (void *(*)()) offsets->Display_get_main;
+    Screen_SetResolution = (void (*)(int, int, bool)) offsets->Screen_SetResolution;
+
     WorldToScreenPoint = (Vector3(*)(void*, Vector3)) offsets->Camera_WorldToScreenPoint;
     Transform_get_position = (Vector3 (*)(void*)) offsets->Transform_get_position;
     get_position = (void (*)(void *, Vector3*)) offsets->Transform_get_position_injected;
